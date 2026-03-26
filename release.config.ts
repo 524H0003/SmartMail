11abb6a6453e81cfc63d4ba1534f1c5f72986883
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-26 10:02:20 +0000

    chore: update

--- release.config.ts
+++ release.config.ts
@@ -8,24 +8,7 @@ const config: GlobalConfig = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", { npmPublish: false }],
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd:
-          "pnpm build && node scripts/release.ts ${nextRelease.version}",
-      },
-    ],
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "release.zip",
-            name: "SmartMail v${nextRelease.version}.zip",
-          },
-        ],
-      },
-    ],
+    "@semantic-release/github",
   ],
 };
 
